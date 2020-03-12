caed73d4a7dbfefa8c244cb30b26bffc72ca018d
NoYoungRea <rea2169@naver.com>
2020-03-12 14:25:48 +0900

    6

--- day4/src/AppStart1.java
+++ day4/src/AppStart1.java
@@ -1,9 +1,7 @@
 
 public class AppStart1 {
 	public static void main(String[]args) {
-		Person p=new Person();
-		System.out.println(p.equals(3));
-		
-		Student p1=new Student();
+		Student s=new Student();
+		s.name="hgd";
 	}
 }
