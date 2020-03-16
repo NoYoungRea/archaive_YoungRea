daaab1a823ead51d5ba72ef8f2575eee6b59c7c3
NoYoungRea <rea2169@naver.com>
2020-03-16 16:24:23 +0900

    11

--- day7/701/src/AppStart.java
+++ day7/701/src/AppStart.java
@@ -4,10 +4,21 @@ public class AppStart {
 
 	public static void main(String[] args) {
 		try {
-		int n = System.in.read();
+			System.out.print("input:");
+			int n=System.in.read();
+			int a=10/0;
+
 		}
 		catch(IOException e) {
 			e.printStackTrace();
+			e.printStackTrace();
+		}
+		catch(ArithmeticException e) {
+			System.out.println("math");
+			e.printStackTrace();
+		}
+		finally {
+			System.out.println("done");
 		}
 	}
 }
